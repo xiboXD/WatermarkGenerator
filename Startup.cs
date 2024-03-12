caa5c9a2b9715fef02028d91408058260f6ad1c6
xibo <xibo@portkey.finance>
2024-03-12 10:39:00 +0800

    feat/change-fail-resp

--- Startup.cs
+++ Startup.cs
@@ -1,64 +1,54 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Extensions.Logging;
-using System;
-using System.IO;
 
-namespace WebApiClient
+namespace WebApiClient;
+
+public class Startup
 {
-    public class Startup
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        // Add framework services.
+        services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+        if (env.IsDevelopment())
         {
-            // Add framework services.
-            services.AddControllers();
-            // Add any additional services your application requires here.
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseStaticFiles(); 
+        app.UseDirectoryBrowser();
+
+        // Create log folder if it doesn't exist
+        var logFolder = Path.Combine(env.ContentRootPath, "logs");
+        if (!Directory.Exists(logFolder))
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseStaticFiles(); // If not already added
-            app.UseDirectoryBrowser(); // If not already added
-
-            // Create log folder if it doesn't exist
-            var logFolder = Path.Combine(env.ContentRootPath, "logs");
-            if (!Directory.Exists(logFolder))
-            {
-                Directory.CreateDirectory(logFolder);
-            }
-
-            // Configure Serilog for logging to file
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.File(Path.Combine(logFolder, $"log-{DateTime.Now:yyyyMMdd}.txt"))
-                .CreateLogger();
-
-
-            // Add Serilog to the logger factory
-            loggerFactory.AddSerilog();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            Directory.CreateDirectory(logFolder);
         }
+
+        // Configure Serilog for logging to file
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(Path.Combine(logFolder, $"log-{DateTime.Now:yyyyMMdd}.txt"))
+            .CreateLogger();
+
+
+        // Add Serilog to the logger factory
+        loggerFactory.AddSerilog();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
     }
 }
--- controllers/WatermarkController.cs
+++ controllers/WatermarkController.cs
@@ -5,96 +5,96 @@ using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Processing;
 using WebApiClient.interfaces;
-using Microsoft.Extensions.Logging;
-using System;
-using System.IO;
 
-namespace WebApiClient.controllers
+namespace WebApiClient.controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ImageController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class ImageController : ControllerBase
+    private readonly ILogger<ImageController> _logger;
+
+    public ImageController(ILogger<ImageController> logger)
     {
-        private readonly ILogger<ImageController> _logger;
+        _logger = logger;
+    }
 
-        public ImageController(ILogger<ImageController> logger)
+    [HttpPost("process")]
+    public IActionResult AddWatermark([FromBody] WatermarkApiSchema.WatermarkRequest request)
+    {
+        try
         {
-            _logger = logger;
-        }
+            // Convert input Base64 string to byte array
+            var inputBytes = Convert.FromBase64String(request.SourceImage.Split(",")[1]);
 
-        [HttpPost("process")]
-        public IActionResult AddWatermark([FromBody] WatermarkApiSchema.WatermarkRequest request)
-        {
-            try
+            // Load the input image from byte array
+            using var image = Image.Load(inputBytes);
+
+            // Define the font and text options for your watermark
+            var fonts = new FontCollection();
+            var fontFamily = fonts.Add("font/PressStart2P-Regular.ttf");
+            var font = fontFamily.CreateFont(10, FontStyle.Regular);
+
+            var text = request.Watermark.Text;
+
+            var textSize = TextMeasurer.MeasureSize(text, new TextOptions(font));
+            var textLocation = new PointF(image.Width - textSize.Width - 10, image.Height - textSize.Height - 10);
+
+            var backgroundRectangle = new RectangularPolygon(textLocation.X - 5, textLocation.Y - 5, textSize.Width + 10, textSize.Height + 10);
+            image.Mutate(x => x.Fill(Color.White.WithAlpha(0.6f), backgroundRectangle));
+
+            // Apply the watermark
+            image.Mutate(x =>
+                x.DrawText(
+                    text,
+                    font,
+                    Color.Black.WithAlpha(0.6f),
+                    textLocation
+                )
+            );
+
+            // Convert the watermarked image to Base64 string
+            var outputBase64 = ConvertToBase64(image);
+
+            var logDetails = new WatermarkApiSchema.LogDetails
             {
-                // Convert input Base64 string to byte array
-                var inputBytes = Convert.FromBase64String(request.SourceImage.Split(",")[1]);
+                Timestamp = $"{DateTimeOffset.Now:yyyy-MM-dd HH:mm:ss.fff zzz}",
+                RequestMethod = "POST",
+                RequestBody = request,
+                RequestUrl = "/image/process",
+                StatusCode = 200,
+                Response = outputBase64
+            };
 
-                // Load the input image from byte array
-                using var image = Image.Load(inputBytes);
+            _logger.LogInformation("Image processed successfully. {@LogDetails}", logDetails);
 
-                // Define the font and text options for your watermark
-                var fonts = new FontCollection();
-                var fontFamily = fonts.Add("font/PressStart2P-Regular.ttf");
-                var font = fontFamily.CreateFont(10, FontStyle.Regular);
-
-                var text = request.Watermark.Text;
-
-                var textSize = TextMeasurer.MeasureSize(text, new TextOptions(font));
-                var textLocation = new PointF(image.Width - textSize.Width - 10, image.Height - textSize.Height - 10);
-
-                var backgroundRectangle = new RectangularPolygon(textLocation.X - 5, textLocation.Y - 5, textSize.Width + 10, textSize.Height + 10);
-                image.Mutate(x => x.Fill(Color.White.WithAlpha(0.6f), backgroundRectangle));
-
-                // Apply the watermark
-                image.Mutate(x =>
-                    x.DrawText(
-                        text,
-                        font,
-                        Color.Black.WithAlpha(0.6f),
-                        textLocation
-                    )
-                );
-
-                // Convert the watermarked image to Base64 string
-                var outputBase64 = ConvertToBase64(image);
-
-                var logDetails = new WatermarkApiSchema.LogDetails
-                {
-                    Timestamp = $"{DateTimeOffset.Now:yyyy-MM-dd HH:mm:ss.fff zzz}",
-                    RequestMethod = "POST",
-                    RequestBody = request,
-                    RequestUrl = "/image/process",
-                    StatusCode = 200,
-                    Response = outputBase64
-                };
-
-                _logger.LogInformation("Image processed successfully. {@logDetails}", logDetails);
-
-                return Ok(new { ProcessedImage = $"data:image/webp;base64,{outputBase64}" });
-            }
-            catch (Exception ex)
-            {
-                var logDetails = new WatermarkApiSchema.LogDetails
-                {
-                    Timestamp = $"{DateTimeOffset.Now:yyyy-MM-dd HH:mm:ss.fff zzz}",
-                    RequestMethod = "POST",
-                    RequestBody = request,
-                    RequestUrl = "/image/process",
-                    StatusCode = 500,
-                    Response = ex.Message
-                };
-                _logger.LogError("An error occurred while processing the image. {@logDetails}", logDetails);
-                return StatusCode(500, $"An error occurred: {ex.Message}");
-            }
+            return Ok(new { ProcessedImage = $"data:image/webp;base64,{outputBase64}" });
         }
-
-        // Convert the image to Base64 string
-        private static string ConvertToBase64(Image image)
+        catch (Exception ex)
         {
-            using var memoryStream = new MemoryStream();
-            image.Save(memoryStream, SixLabors.ImageSharp.Formats.Webp.WebpFormat.Instance);
-            return Convert.ToBase64String(memoryStream.ToArray());
+            var logDetails = new WatermarkApiSchema.LogDetails
+            {
+                Timestamp = $"{DateTimeOffset.Now:yyyy-MM-dd HH:mm:ss.fff zzz}",
+                RequestMethod = "POST",
+                RequestBody = request,
+                RequestUrl = "/image/process",
+                StatusCode = 500,
+                Response = ex.Message
+            };
+            _logger.LogError("An error occurred while processing the image. {@LogDetails}", logDetails);
+            var errorResponse = new
+            {
+                error = ex.Message
+            };
+            return StatusCode(500, errorResponse);
         }
+    }
+
+    // Convert the image to Base64 string
+    private static string ConvertToBase64(Image image)
+    {
+        using var memoryStream = new MemoryStream();
+        image.Save(memoryStream, SixLabors.ImageSharp.Formats.Webp.WebpFormat.Instance);
+        return Convert.ToBase64String(memoryStream.ToArray());
     }
 }
